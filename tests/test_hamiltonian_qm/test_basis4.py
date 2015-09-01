b58f8059c960fcfd3623c0b03d5116ef3d8c6d62
Alexey V. Akimov <alexvakimov@gmail.com>
2015-09-01 14:54:48 -0400

    Preparing to create a test-tutorial showing how to do INDO calculations on selected molecule

---
+++ tests/test_hamiltonian_qm/test_basis4.py
@@ -0,0 +1,137 @@
+#*********************************************************************************
+#* Copyright (C) 2015 Alexey V. Akimov
+#*
+#* This file is distributed under the terms of the GNU General Public License
+#* as published by the Free Software Foundation, either version 2 of
+#* the License, or (at your option) any later version.
+#* See the file LICENSE in the root directory of this distribution
+#* or <http://www.gnu.org/licenses/>.
+#*
+#*********************************************************************************/
+
+###################################################################
+# Tutorial: AO basis construction & AO to atoms (and reverse) mappings are taken care of in built-in function
+# this helps to simplify script and hide standard details
+# also print overlaps matrix
+###################################################################
+
+import os
+import sys
+import math
+
+# Fisrt, we add the location of the library to test to the PYTHON path
+cwd = os.getcwd()
+print "Current working directory", cwd
+sys.path.insert(1,cwd+"/../../_build/src/mmath")
+sys.path.insert(1,cwd+"/../../_build/src/chemobjects")
+sys.path.insert(1,cwd+"/../../_build/src/hamiltonian")
+sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic")
+sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM")
+sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Control_Parameters")
+sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Model_Parameters")
+sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Basis_Setups")
+sys.path.insert(1,cwd+"/../../_build/src/dyn")
+sys.path.insert(1,cwd+"/../../_build/src/qchem/qobjects")
+sys.path.insert(1,cwd+"/../../_build/src/qchem/basis")
+sys.path.insert(1,cwd+"/../../_build/src/converters")
+
+print "\nTest 1: Importing the library and its content"
+from cygmmath import *
+from cygchemobjects import *
+from cyghamiltonian import *
+from cygcontrol_parameters import *
+from cygmodel_parameters import *
+from cygbasis_setups import *
+from cygdyn import *
+from cygqobjects import *
+from cygbasis import *
+from cygconverters import *
+
+from LoadPT import * # Load_PT
+from LoadMolecule import * # Load_Molecule
+
+
+
+#=========== STEP 1:  Create Universe and populate it ================
+U = Universe()
+Load_PT(U, "elements.dat", 0)
+
+
+#=========== STEP 2:  Create system and load a molecule ================
+syst = System()
+Load_Molecule(U, syst, os.getcwd()+"/1water.ent", "pdb")
+
+print "Number of atoms in the system = ", syst.Number_of_atoms
+atlst1 = range(0,syst.Number_of_atoms)
+
+
+#=========== STEP 3: Create control parameters (setting computation options) ================
+prms = Control_Parameters()
+get_parameters_from_file("control_parameters.dat", prms)
+print "guess type = ", prms.guess_type
+
+
+#=========== STEP 4:  Create model parameters and load them from file (using control parameters options) ================
+modprms = Model_Parameters()
+
+# Initialize/read model parameters (need basis info)
+print "Setting parameters"
+if(prms.hamiltonian=="eht" or prms.hamiltonian=="geht"):
+    set_parameters_eht(prms, modprms)
+elif(prms.hamiltonian=="indo"):
+    set_parameters_indo(prms, modprms);
+elif(prms.hamiltonian=="geht1"):
+    set_parameters_geht1(prms, modprms); 
+elif(prms.hamiltonian=="geht2"):
+    set_parameters_geht2(prms, modprms); 
+
+
+#=========== STEP 5: Set basis (STO-3G_DZ) ================
+Nelec = 0;
+Norb = 0;
+
+#------- Input --------------
+mol_at_types = StringList()
+R = VECTORList()
+for i in xrange(syst.Number_of_atoms):
+    mol_at_types.append(syst.Atoms[i].Atom_element)
+    R.append(syst.Atoms[i].Atom_RB.rb_cm)
+
+#-------- Output -----------
+basis = AOList()
+atom_to_ao_map = intMap()
+ao_to_atom_map = intList()
+
+
+verb = 0
+basis, Nelec, Norb, atom_to_ao_map, ao_to_atom_map = set_basis_STO_3G_DZ(mol_at_types, R, modprms, verb)
+
+
+#=========== STEP 6: Depending on hamiltonian to use, set internal parameters ================
+
+if(prms.hamiltonian=="eht" or prms.hamiltonian=="geht" or prms.hamiltonian=="geht1" or prms.hamiltonian=="geht2"):
+    set_parameters_eht_mapping(modprms, basis)
+    set_parameters_eht_mapping1(modprms,syst.Number_of_atoms,mol_at_types)
+
+
+
+#=========== STEP 7: Overlap matrix ================
+
+Sao = MATRIX(Norb, Norb)
+x_period = 0
+y_period = 0
+z_period = 0
+t1 = VECTOR()
+t2 = VECTOR()
+t3 = VECTOR()
+
+
+update_overlap_matrix(x_period, y_period, z_period, t1, t2, t3, basis, Sao);
+
+print "AO overlap matrix"
+Sao.show_matrix()
+
+
+
+
+
