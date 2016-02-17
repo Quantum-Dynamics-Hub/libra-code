ba6279ecb80cbd1afc760391304e6441c8606166
Alexey V Akimov <alexvakimov@gmail.com>
2016-02-17 16:05:06 -0500

    Reorganized files in the test_qm directory. Also add new set of calculations - for Si quantum dots

--- tests/test_qm/tests_Si_QD/test_qd.py
+++ tests/test_qm/tests_Si_QD/test_qd.py
@@ -19,20 +19,20 @@ import math
 # Fisrt, we add the location of the library to test to the PYTHON path
 cwd = os.getcwd()
 print "Current working directory", cwd
-sys.path.insert(1,cwd+"/../../_build/src/mmath")
-sys.path.insert(1,cwd+"/../../_build/src/chemobjects")
-sys.path.insert(1,cwd+"/../../_build/src/hamiltonian")
-sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic")
-sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM")
-sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Control_Parameters")
-sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Model_Parameters")
-sys.path.insert(1,cwd+"/../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Basis_Setups")
-sys.path.insert(1,cwd+"/../../_build/src/dyn")
-sys.path.insert(1,cwd+"/../../_build/src/qchem/qobjects")
-sys.path.insert(1,cwd+"/../../_build/src/qchem/basis")
-sys.path.insert(1,cwd+"/../../_build/src/converters")
-sys.path.insert(1,cwd+"/../../_build/src/calculators")
-sys.path.insert(1,cwd+"/../../_build/src/qchem_tools")
+sys.path.insert(1,cwd+"/../../../_build/src/mmath")
+sys.path.insert(1,cwd+"/../../../_build/src/chemobjects")
+sys.path.insert(1,cwd+"/../../../_build/src/hamiltonian")
+sys.path.insert(1,cwd+"/../../../_build/src/hamiltonian/Hamiltonian_Atomistic")
+sys.path.insert(1,cwd+"/../../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM")
+sys.path.insert(1,cwd+"/../../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Control_Parameters")
+sys.path.insert(1,cwd+"/../../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Model_Parameters")
+sys.path.insert(1,cwd+"/../../../_build/src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_QM/Basis_Setups")
+sys.path.insert(1,cwd+"/../../../_build/src/dyn")
+sys.path.insert(1,cwd+"/../../../_build/src/qchem/qobjects")
+sys.path.insert(1,cwd+"/../../../_build/src/qchem/basis")
+sys.path.insert(1,cwd+"/../../../_build/src/converters")
+sys.path.insert(1,cwd+"/../../../_build/src/calculators")
+sys.path.insert(1,cwd+"/../../../_build/src/qchem_tools")
 
 print "\nTest 1: Importing the library and its content"
 from cygmmath import *
@@ -61,13 +61,12 @@ Load_PT(U, "elements.dat", 1)
 
 #=========== STEP 2:  Create system and load a molecule ================
 syst = System()
-Load_Molecule(U, syst, os.getcwd()+"/azulene.pdb", "iqmol_pdb")
+Load_Molecule(U, syst, os.getcwd()+"/QD_6_h.inp.xyz", "xyz")
 #Load_Molecule(U, syst, os.getcwd()+"/benzene.pdb", "pdb_1")
 #Load_Molecule(U, syst, os.getcwd()+"/c.pdb", "pdb_1")
 #Load_Molecule(U, syst, os.getcwd()+"/c2.pdb", "pdb_1")
 #Load_Molecule(U, syst, os.getcwd()+"/ch4.pdb", "pdb_1")
 
-#sys.exit(0)
 
 print "Number of atoms in the system = ", syst.Number_of_atoms
 atlst1 = range(0,syst.Number_of_atoms)
@@ -114,13 +113,11 @@ basis_ao, Nelec, Norb, atom_to_ao_map, ao_to_atom_map = set_basis_STO_3G_DZ(mol_
 
 
 #=========== STEP 6: Depending on hamiltonian to use, set internal parameters ================
-#sys.exit(0)
 
 if(prms.hamiltonian=="eht"):
     set_parameters_eht_mapping(modprms, basis_ao)    
     set_parameters_eht_mapping1(modprms,syst.Number_of_atoms,mol_at_types)
 
-#sys.exit(0)
 
 #=========== STEP 7: Overlap matrix ================
 
@@ -131,19 +128,15 @@ z_period = 0
 t1 = VECTOR()
 t2 = VECTOR()
 t3 = VECTOR()
-
-
-
 update_overlap_matrix(x_period, y_period, z_period, t1, t2, t3, basis_ao, Sao);
-print "AO overlap matrix"
-Sao.show_matrix()
+
 
 #=========== STEP 8: Parameters ================
 eri = doubleList()
 V_AB = doubleList()
 opt = 1  # 1 - for INDO, 0 - for CNDO/CNDO2
 Hao = MATRIX(Norb, Norb)
-debug = 1
+debug = 0
      
 if(prms.hamiltonian=="indo"):
     Sao.Init_Unit_Matrix(1.0);  
@@ -153,10 +146,6 @@ if(prms.hamiltonian=="indo"):
 elif(prms.hamiltonian=="eht"):
     Hamiltonian_core_eht(syst, basis_ao, prms, modprms, atom_to_ao_map, ao_to_atom_map, Hao,  Sao, debug)
 
-print "Core Hamiltonian"
-Hao.show_matrix()
-
-#sys.exit(0)
 
 Nelec_alp = Nelec/2
 Nelec_bet = Nelec - Nelec_alp
@@ -170,35 +159,6 @@ pop_opt = 0  #  0 -  integer populations,  1 - Fermi distribution
 
 res_alp = Fock_to_P(Hao, Sao, Nelec_alp, degen, kT, etol, pop_opt)
 res_bet = Fock_to_P(Hao, Sao, Nelec_bet, degen, kT, etol, pop_opt)
-
-
-print "Eigenvalues (alp):\n"
-res_alp[0].show_matrix()
-print "Eigenvalues (bet):\n"
-res_bet[0].show_matrix()
-
-print "Eigenvectors(alp):\n"
-res_alp[1].show_matrix()
-print "Eigenvectors(bet):\n"
-res_bet[1].show_matrix()
-
-
-print "Density matrix(alp):\n"
-res_alp[2].show_matrix()
-print "Density matrix(bet):\n"
-res_bet[2].show_matrix()
-
-
-print "Bands(alp):\n"
-print res_alp[3]
-print "Bands(bet):\n"
-print res_bet[3]
-
-
-print "Occupations(alp):\n"
-print res_alp[4]
-print "Occupations(bet):\n"
-print res_bet[4]
 
 
 
@@ -217,11 +177,7 @@ elif(prms.hamiltonian=="eht"):
     Hamiltonian_Fock_eht(el, syst, basis_ao, prms, modprms, atom_to_ao_map, ao_to_atom_map)
 
 
-print "Fock matrix at first iteration (alp)"
-el.get_Fao_alp().show_matrix()
-
 #===============  Now to SCF iterations =======================
-
 E = scf(el, syst, basis_ao, prms, modprms, atom_to_ao_map, ao_to_atom_map, 0); 
 
 
@@ -239,17 +195,17 @@ for j in xrange(Norb):
      print "%5.3i %12.8f   %12.8f  %12.8f   %12.8f" % (j, res_alp[3][j][1], res_alp[4][j][1], res_bet[3][j][1], res_bet[4][j][1])
 
 
-vlist = intList()    
-vlist.append(23)
-vlist.append(24)
+#vlist = intList()    
+#vlist.append(23)
+#vlist.append(24)
 
-prms.orbs = vlist
+#prms.orbs = vlist
 prms.nx_grid = 40
 prms.ny_grid = 40
 prms.nz_grid = 40
-prms.charge_density_prefix = "char_dens"
+#prms.charge_density_prefix = "char_dens"
 
-charge_density( el, syst, basis_ao, prms)
+#charge_density( el, syst, basis_ao, prms)
 
 
 
