1030d31469a312438cea538c13353466a1f7037a
Alexey V Akimov <alexvakimov@gmail.com>
2016-04-10 18:16:12 -0400

    Updated the test-tutorial file that shows how to use non-core (Python) scripts included in Libra

--- tests/test_libra_py/test1.py
+++ tests/test_libra_py/test1.py
@@ -1,5 +1,10 @@
+import sys
 
-from cyglibra_core import *
+if sys.platform=="cygwin":
+    from cyglibra_core import *
+elif sys.platform=="linux" or sys.platform=="linux2":
+    from liblibra_core import *
+
 from libra_py import *
 
 label = ["H"]
