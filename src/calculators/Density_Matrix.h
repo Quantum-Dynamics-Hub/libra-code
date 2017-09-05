dd7d3d0961888e953319b95210cdc2e166542a4a
Alexey <alexvakimov@gmail.com>
2017-09-05 12:56:01 +0300

    Added the complex-valued version of the Fock_to_P and compute_density_matrix functions

--- src/calculators/Density_Matrix.h
+++ src/calculators/Density_Matrix.h
@@ -32,22 +32,44 @@ using namespace libmeigen;
 namespace libcalculators{
 
 void compute_density_matrix(vector< pair<int,double> >& occ, MATRIX* C, MATRIX* P);
+void compute_density_matrix(vector< pair<int,double> >& occ, CMATRIX* C, CMATRIX* P);
+
 MATRIX compute_density_matrix(boost::python::list occ, MATRIX C);
+CMATRIX compute_density_matrix(boost::python::list occ, CMATRIX C);
 
 void Fock_to_P(int Norb,int Nocc, int degen, double Nel, std::string eigen_method, int pop_opt,
                MATRIX* Fao, MATRIX* Sao, MATRIX* C, MATRIX* E,
                vector< pair<int,double> >& bands, vector< pair<int,double> >& occ,
                MATRIX* P, vector<Timer>& bench_t);
+void Fock_to_P(int Norb,int Nocc, int degen, double Nel, std::string eigen_method, int pop_opt,
+               CMATRIX* Fao, CMATRIX* Sao, CMATRIX* C, CMATRIX* E,
+               vector< pair<int,double> >& bands, vector< pair<int,double> >& occ,
+               CMATRIX* P, vector<Timer>& bench_t);
 
+
+// Versions with the optional benchmark
 void Fock_to_P(MATRIX* Fao, MATRIX* Sao, double Nel, double degen, double kT, double etol, int pop_opt, /*Inputs*/
                MATRIX* E, MATRIX* C, MATRIX* P,                                              /*Outputs*/
                vector< pair<int,double> >& bands, vector< pair<int,double> >& occ,           /*Outputs*/
                int BM, vector<Timer>& bench_t);                                              /*Benchmarking data*/
+void Fock_to_P(CMATRIX* Fao, CMATRIX* Sao, double Nel, double degen, double kT, double etol, int pop_opt, /*Inputs*/
+               CMATRIX* E, CMATRIX* C, CMATRIX* P,                                              /*Outputs*/
+               vector< pair<int,double> >& bands, vector< pair<int,double> >& occ,           /*Outputs*/
+               int BM, vector<Timer>& bench_t);                                              /*Benchmarking data*/
+
+// Versions without the benchmark
 void Fock_to_P(MATRIX* Fao, MATRIX* Sao, double Nel, double degen, double kT, double etol, int pop_opt, /*Inputs*/
                MATRIX* E, MATRIX* C, MATRIX* P,                                                         /*Outputs*/
                vector< pair<int,double> >& bands, vector< pair<int,double> >& occ                       /*Outputs*/
               );
+void Fock_to_P(CMATRIX* Fao, CMATRIX* Sao, double Nel, double degen, double kT, double etol, int pop_opt, /*Inputs*/
+               CMATRIX* E, CMATRIX* C, CMATRIX* P,                                                         /*Outputs*/
+               vector< pair<int,double> >& bands, vector< pair<int,double> >& occ                       /*Outputs*/
+              );
+
+// Version for the Python
 boost::python::list Fock_to_P(MATRIX Fao, MATRIX Sao, double Nel, double degen, double kT, double etol, int pop_opt);
+boost::python::list Fock_to_P(CMATRIX Fao, CMATRIX Sao, double Nel, double degen, double kT, double etol, int pop_opt);
 
 
 
