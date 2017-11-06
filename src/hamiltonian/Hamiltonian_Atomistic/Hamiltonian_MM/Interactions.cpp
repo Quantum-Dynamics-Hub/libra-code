ac81076b26931b6b9daa7b3e91344d9cc219eed2
Alexey <alexvakimov@gmail.com>
2017-11-06 15:27:31 -0500

    Started re-write of the classical MM Hamiltonian - for ease of periodic calculations with bonded interactions, and for simpler user-friendly interface. The old methods are still here, but my hope is that the new classes will be easier to use, so that they may get more of usage later

---
+++ src/hamiltonian/Hamiltonian_Atomistic/Hamiltonian_MM/Interactions.cpp
@@ -0,0 +1,159 @@
+/*********************************************************************************
+* Copyright (C) 2017 Alexey V. Akimov
+*
+* This file is distributed under the terms of the GNU General Public License
+* as published by the Free Software Foundation, either version 2 of
+* the License, or (at your option) any later version.
+* See the file LICENSE in the root directory of this distribution
+* or <http://www.gnu.org/licenses/>.
+*
+*********************************************************************************/
+/**
+  \file Interactions.cpp
+  \brief The file implements functions for molecular-mechanical Hamiltonian calculations as well
+  as the classes for organizing such computations in an object-oriented way: basic functionality of the class
+*/
+
+#include "Interactions.h"
+
+/// liblibra namespace
+namespace liblibra{
+
+
+/// libhamiltonian namespace
+namespace libhamiltonian{
+
+/// libhamiltonian_atomistic namespace
+namespace libhamiltonian_atomistic{
+
+/// libhamiltonian_mm namespace
+namespace libhamiltonian_mm{
+
+
+map<std::string, double> dict2map(boost::python::dict d){
+
+  std::string key;
+  double value;
+  map<std::string, double> res;
+
+  for(int i=0;i<len(d.values());i++){
+    key = extract<std::string>(d.keys()[i]);
+    value = extract<double>(d.values()[i]);
+
+    res.insert( std::pair<std::string, double>(key, value) ); 
+  }
+
+  return res;
+}
+
+
+//========================================================
+// Basic functionality of the Interaction class
+//========================================================
+
+Interaction_N_Body::Interaction_N_Body(){  
+/**
+  Do nothing
+*/
+    Nbody = 0;
+
+    is_active = 1;    // set it up to active by default
+    int_type = -1;    // Undefined
+    functional = -1;  // Undefined 
+
+    energy = 0.0;     // Energy 
+    hessian = 0.0;    // Hessian
+    stress_at = 0.0;  // stress tensor
+
+
+}
+
+Interaction_N_Body::Interaction_N_Body(int Nbody_){  
+/**
+  Allocates memory for the pointers
+*/
+
+    Nbody = Nbody_;
+
+    r = vector<VECTOR*>(Nbody, NULL);
+    t = vector<VECTOR*>(Nbody, NULL);
+    f = vector<VECTOR*>(Nbody, NULL);
+    q = vector<double*>(Nbody, NULL);
+
+
+    is_active = 1;    // set it up to active by default
+    int_type = -1;    // Undefined
+    functional = -1;  // Undefined 
+
+    energy = 0.0;     // Energy 
+    hessian = 0.0;    // Hessian
+    stress_at = 0.0;  // stress tensor
+
+}
+
+
+void Interaction_N_Body::set_coords(vector<VECTOR*>& r_, vector<int>& indxs){
+
+    address_subset<VECTOR>(r, r_, indxs);
+
+}// set_coords
+
+void Interaction_N_Body::set_coords(vector<VECTOR>& r_, vector<int>& indxs){
+
+    address_subset<VECTOR>(r, r_, indxs);
+
+}// set_coords
+
+
+void Interaction_N_Body::set_transl(vector<VECTOR*>& t_, vector<int>& indxs){
+
+    address_subset<VECTOR>(t, t_, indxs);
+
+}// set_transl
+
+void Interaction_N_Body::set_transl(vector<VECTOR>& t_, vector<int>& indxs){
+
+    address_subset<VECTOR>(t, t_, indxs);
+
+}// set_transl
+
+
+void Interaction_N_Body::set_forces(vector<VECTOR*>& f_, vector<int>& indxs){
+
+    address_subset<VECTOR>(f, f_, indxs);
+
+}// set_forces
+
+void Interaction_N_Body::set_forces(vector<VECTOR>& f_, vector<int>& indxs){
+
+    address_subset<VECTOR>(f, f_, indxs);
+
+}// set_forces
+
+
+void Interaction_N_Body::set_charges(vector<double*>& q_, vector<int>& indxs){
+
+    address_subset<double>(q, q_, indxs);
+
+}// set_charges
+
+void Interaction_N_Body::set_charges(vector<double>& q_, vector<int>& indxs){
+
+    address_subset<double>(q, q_, indxs);
+
+}// set_charges
+
+
+void Interaction_N_Body::set_functional(std::string f){
+ ;;
+}
+
+
+
+
+
+
+}// namespace libhamiltonian_mm
+}// namespace libhamiltonian_atomistic
+}// namespace libhamiltonian
+}// liblibra
