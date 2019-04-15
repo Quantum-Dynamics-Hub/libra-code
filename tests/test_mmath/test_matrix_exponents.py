2eac3ea0655fda1751e2a2c43d40730013b3a6e4
Alexey V. Akimov <alexvakimov@gmail.com>
2019-04-14 20:50:54 -0400

    Doc Challenge Day 78: Added an example of computing exponent of a non-Hermitian matrix, also added a doc with theory on this

--- tests/test_mmath/test_matrix_exponents.py
+++ tests/test_mmath/test_matrix_exponents.py
@@ -12,6 +12,7 @@
 import os
 import sys
 import math
+import cmath
 
 # Fisrt, we add the location of the library to test to the PYTHON path
 if sys.platform=="cygwin":
@@ -86,7 +87,7 @@ eH2.show_matrix()
 
 print "============ non-Hermitian Matrix =============="
 H = CMATRIX(2,2)
-H.set(0,0, -0.001+0.0j);         H.set(0,1, 0.001+0.001j)
+H.set(0,0, -0.001+0.0j);         H.set(0,1, 0.002+0.003j)
 H.set(1,0, 0.001+0.001j);        H.set(1,1,  0.002+0.0j)
 print "H = \n"; H.show_matrix()
 
@@ -102,7 +103,7 @@ eH1 = exp_(H, 0.5*dt)
 eH1 = eH1 * eH1
 eH1.show_matrix()
 
-print "Method 2"
+print "Method 2 and 2a in principle work even for non-Hermitian matrices"
 eH2 = exp_2(H, dt)
 eH2.show_matrix()
 
@@ -111,8 +112,87 @@ eH2 = exp_2(H, 0.5*dt)
 eH2 = eH2 * eH2
 eH2.show_matrix()
 
-#print "Method 2, only 2 terms"
-#eH2 = exp_2(H, dt, 10000, 1e-10)
-#eH2.show_matrix()
 
+print "Method 3 is based on eigendecomposition"
+R = CMATRIX(2,2);  R_dag = CMATRIX(2,2);  iR = CMATRIX(2,2)
+L = CMATRIX(2,2);  L_dag = CMATRIX(2,2);  iL_dag = CMATRIX(2,2)
+ER = CMATRIX(2,2); EL_dag = CMATRIX(2,2)
+
+# H * R = R * E
+solve_eigen_nosort(H, ER, R, 0)
+R_dag = R.H()
+
+H_dag = H.H()
+solve_eigen_nosort(H_dag, EL_dag, L, 0)
+L_dag = L.H()
+
+FullPivLU_inverse(R, iR)
+FullPivLU_inverse(L_dag, iL_dag)
+
+print "ER = "; ER.show_matrix()
+print "EL_dag = "; EL_dag.show_matrix()
+
+D = L.H() * R;
+
+print "L.H() * R = "; D.show_matrix()
+print "|D(0,0)| = ", abs(D.get(0,0))
+print "|D(1,1)| = ", abs(D.get(1,1))
+
+
+eE3 = CMATRIX(2,2)
+for i in xrange(2):
+    eE3.set(i,i,  cmath.exp(dt*ER.get(i,i)) )
+
+print "exp(dt*ER) = "; eE3.show_matrix()
+
+exH3 = iL_dag * D * eE3 * iR
+
+print "exp(dt*H) = "; exH3.show_matrix()
+
+#sys.exit(0)
+
+
+print "============ non-Hermitian Matrix =============="
+H = CMATRIX(3,3)
+H.set(0,0, -0.001+0.0j);         H.set(0,1, 0.002+0.003j);   H.set(0,2, 0.002+0.003j)
+H.set(1,0, 0.001+0.001j);        H.set(1,1,  0.002+0.0j);    H.set(1,2,  -0.002+0.1j)
+H.set(2,0, 0.001-0.001j);        H.set(2,1,  0.002+0.1j);    H.set(2,2,   0.004+0.0j)
+print "H = \n"; H.show_matrix()
+
+R = CMATRIX(3,3);  R_dag = CMATRIX(3,3); iR = CMATRIX(3,3)
+L = CMATRIX(3,3);  L_dag = CMATRIX(3,3); iL_dag = CMATRIX(3,3)
+ER = CMATRIX(3,3); EL_dag = CMATRIX(3,3)
+
+# H * R = R * E
+solve_eigen_nosort(H, ER, R, 0)
+R_dag = R.H()
+
+H_dag = H.H()
+solve_eigen_nosort(H_dag, EL_dag, L, 0)
+L_dag = L.H()
+
+FullPivLU_inverse(R, iR)
+FullPivLU_inverse(L_dag, iL_dag)
+
+print "ER = "; ER.show_matrix()
+print "EL_dag = "; EL_dag.show_matrix()
+D = L.H() * R
+print "L.H() * R = "; D.show_matrix()
+print "|D(0,0)| = ", abs(D.get(0,0))
+print "|D(1,1)| = ", abs(D.get(1,1))
+print "|D(2,2)| = ", abs(D.get(2,2))
+
+eE3 = CMATRIX(3,3)
+for i in xrange(3):
+    eE3.set(i,i,  cmath.exp(dt*ER.get(i,i)) )
+
+print "exp(dt*ER) = "; eE3.show_matrix()
+
+exH3 = iL_dag * D * eE3 * iR
+print "exp(dt*H) = "; exH3.show_matrix()
+
+
+print "Method 2:"
+eH2 = exp_2(H, dt)
+eH2.show_matrix()
 
