dca80936d3773953ba0cf84927c0f2205157b5d6
Alexey V. Akimov <alexvakimov@gmail.com>
2019-04-30 22:51:39 -0400

    Doc Challenge Day 94: Added a complete example of running the NAMD of adamantane with Libra-ErgoSCF

---
+++ notebooks/Example9b_NAMD_adamantane/step1/convert.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import math
+import re
+
+if sys.platform=="cygwin":
+    from cyglibra_core import *
+elif sys.platform=="linux" or sys.platform=="linux2":
+    from liblibra_core import *
+
+from libra_py import DFTB_methods
+
+
+DFTB_methods.xyz_traj2gen_sp("adamantane.xyz", "adamantane.gen", 0, "C")
