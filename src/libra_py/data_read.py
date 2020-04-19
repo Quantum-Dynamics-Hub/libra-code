5a03929598f82dbeae8db72a1ea999eda0adea30
MohammadShakiba <43968475+MohammadShakiba@users.noreply.github.com>
2020-04-19 23:32:49 +0430

    Update data_read.py

--- src/libra_py/data_read.py
+++ src/libra_py/data_read.py
@@ -21,6 +21,7 @@ import os
 import sys
 import math
 import copy
+import numpy as np
 
 if sys.platform=="cygwin":
     from cyglibra_core import *
@@ -290,3 +291,51 @@ def get_data_from_file2(filename, cols):
 
 
 
+def read_cube(filename: str):
+    """
+    This function reads the wavefunction from a cube file and stores it in
+    a 1D numpy array
+
+    Args:
+
+        filename ( string ): the name of the .cube file to read
+
+    Returns:
+
+        numpy.array : the 1D array of the wavefunctions for all the points on the grid
+    
+
+    Note: 
+
+        Previously, it was called `read_1`
+
+    """
+    
+    f = open(filename,'r')
+    lines = f.readlines()
+    f.close()
+
+    natoms = int(lines[2].split()[0])
+    # We skip a few lines in the cube files, that go as follows:
+    # 2 lines - comments
+    # 1 line  - the number of atoms, etc.
+    # 3 lines - the grid spacing and number of grid points in each dimensions
+
+    nstart = natoms+2+1+3        # the index of the first line containing wfc data
+    nlines = len(lines)          # the total number of lines
+
+    x = []
+    for i in range(nstart,nlines):
+        tmp = lines[i].split()
+        ncols = len(tmp)
+
+        for j in range(ncols):
+            x.append(float(tmp[j]))
+    
+    data = np.array(x)
+#     data = np.loadtxt(filename,skiprows=n)
+    
+    return data
+
+   
+   
