94c4c346e6323bd9b97fe3e7a3f460aaf21d0879
Alexey V. Akimov <alexvakimov@gmail.com>
2020-06-17 13:47:36 -0400

    Edited the copyright notices and changed the file mode

--- src/libra_py/CP2K_methods.py
+++ src/libra_py/CP2K_methods.py
@@ -1,5 +1,4 @@
 #*********************************************************************************
-#*
 #* Copyright (C) 2020 Mohammad Shakiba, Brendan Smith, Alexey V. Akimov
 #* This file is distributed under the terms of the GNU General Public License
 #* as published by the Free Software Foundation, either version 3 of
