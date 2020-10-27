147d239604c56e18cd657fc5d68fc4106a7f1a5f
Alexey V. Akimov <alexvakimov@gmail.com>
2020-10-27 18:28:09 -0400

    Added even more general Holstein model, with the position-dependent couplings

--- src/libra_py/models/Holstein.py
+++ src/libra_py/models/Holstein.py
@@ -384,3 +384,95 @@ def Holstein4(q, params, full_id):
     obj.dc1_dia = dc1_dia
 
     return obj
+
+
+
+
+def Holstein5(q, params, full_id):
+    """
+    n-state model
+
+    H_nn = E_n + 0.5*k*(x-x_n)^2
+    H_n,m = V_n,m * exp(- alp_n,m * (x-x_nm)^2 )
+
+    Args:
+        q ( MATRIX(1,1) ): coordinates of the particle, ndof = 1
+        params ( dictionary ): model parameters
+
+            * **params["E_n"]** ( list of doubles ):  [ default: [0.0, 0.001, 0.001, 0.001], units: Ha]
+            * **params["x_n"]** ( list of doubles ):  [ default: [0.0, 1.0, 1.0, 1.0], units: Bohr]
+            * **params["k_n"]** ( list of doubles ):  [ default: [0.001, 0.001, 0.001, 0.001], units: Ha/Bohr^2]
+            * **params["V"]**   ( list of lists of double ):  [ default:  [[0.001]*4]*4, units: Ha]
+            * **params["alpha"]** ( list of lists of double ):  [ default:  [[0.0]*4]*4, units: 1/Bohr^2]
+            * **params["x_nm"]**  ( list of lists of double ):  [ default:  [[0.0]*4]*4, units: Bohr]
+
+    Returns:
+        PyObject: obj, with the members:
+
+            * obj.ham_dia ( CMATRIX(4,4) ): diabatic Hamiltonian
+            * obj.ovlp_dia ( CMATRIX(4,4) ): overlap of the basis (diabatic) states [ identity ]
+            * obj.d1ham_dia ( list of 1 CMATRIX(4,4) objects ):
+                derivatives of the diabatic Hamiltonian w.r.t. the nuclear coordinate
+            * obj.dc1_dia ( list of 1 CMATRIX(4,4) objects ): derivative coupling in the diabatic basis [ zero ]
+
+    """
+
+    critical_params = ["E_n", "x_n", "k_n" ]
+    default_params = { "V": [ [0.001, 0.001, 0.001, 0.001], [0.001, 0.001, 0.001, 0.001],
+                              [0.001, 0.001, 0.001, 0.001], [0.001, 0.001, 0.001, 0.001] ],
+                       "alpha": [ [0.00, 0.00, 0.00, 0.00], [0.00, 0.00, 0.00, 0.00],
+                                  [0.00, 0.00, 0.00, 0.00], [0.00, 0.00, 0.00, 0.00] ],
+                       "x_nm": [ [0.00, 0.00, 0.00, 0.00], [0.00, 0.00, 0.00, 0.00],
+                                 [0.00, 0.00, 0.00, 0.00], [0.00, 0.00, 0.00, 0.00] ],
+                     }
+    comn.check_input(params, default_params, critical_params)
+
+    E_n = params["E_n"]
+    x_n = params["x_n"]
+    k_n = params["k_n"]
+    V = params["V"]
+    alpha = params["alpha"]
+    x_nm = params["x_nm"]
+
+    n = len(E_n)
+
+    Hdia = CMATRIX(n,n)
+    Sdia = CMATRIX(n,n)
+    d1ham_dia = CMATRIXList();  d1ham_dia.append( CMATRIX(n,n) )
+    dc1_dia = CMATRIXList();  dc1_dia.append( CMATRIX(n,n) )
+
+    Id = Cpp2Py(full_id)
+    indx = Id[-1]
+
+    x = q.col(indx).get(0)
+
+    Sdia.identity()
+
+    for i in range(n):
+        Hdia.set(i,i,  (E_n[i] + 0.5*k_n[i]*(x - x_n[i])**2) * (1.0+0.0j) )
+
+    for i in range(n):
+        for j in range(n):
+            if i!=j:
+                Hdia.set(i,j,  V[i][j] * math.exp(-alpha[i][j] * (x-x_nm[i][j])**2 ) * (1.0+0.0j) )
+
+    for k in [0]:
+        #  d Hdia / dR_0
+        for i in range(n):
+            d1ham_dia[k].set(i,i, (k_n[i] * (x - x_n[i]))*(1.0+0.0j) )
+
+    for k in [0]:
+        for i in range(n):
+            for j in range(n):
+                if i!=j:
+                    d1ham_dia[k].set(i,j,  -2.0*alpha[i][j] * (x-x_nm[i][j]) * V[i][j] * math.exp(-alpha[i][j] * (x-x_nm[i][j])**2 ) * (1.0+0.0j) )
+
+
+
+    obj = tmp()
+    obj.ham_dia = Hdia
+    obj.ovlp_dia = Sdia
+    obj.d1ham_dia = d1ham_dia
+    obj.dc1_dia = dc1_dia
+
+    return obj
