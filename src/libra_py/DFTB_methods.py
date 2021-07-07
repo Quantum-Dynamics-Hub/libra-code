f3bf11ba15629cd1083c29ca7964cc9b71d4790c
Alexey V. Akimov <alexvakimov@gmail.com>
2021-07-06 20:03:40 -0400

    Added an auxiliary function to conver DFTB+ xyz trajectory files to a more standard xyz trajectory file format

--- src/libra_py/DFTB_methods.py
+++ src/libra_py/DFTB_methods.py
@@ -841,3 +841,38 @@ def read_dftbplus_TRA_file( params ):
     return excitation_energies[0:number_of_states], ci_basis, ci_coefficients, spin_components
 
 
+
+
+def dftb_traj2xyz_traj(in_filename, out_filename):    
+    """
+    This is an auxiliary function to convert the DFTB+-generated extended xyz trajectory file
+    to a more coomon xyz trajectory file format
+
+    Args:
+        in_filename ( string ):  the name of the input file
+        out_filename ( string ):  the name of the putput file
+
+    Example:
+        dftb_traj2xyz_traj("md.xyz", "md_reduced.xyz")
+
+    """
+
+    f = open(in_filename, "r")
+    A = f.readlines()
+    f.close()
+
+    f = open(out_filename, "w")
+    for line in A:
+        tmp = line.split()
+        res = line
+
+        if len(tmp) == 8:
+            res = F"  {tmp[0]}  {tmp[1]}  {tmp[2]}  {tmp[3]}\n"
+
+        f.write(res)
+    f.close()
+
+
+
+
+
