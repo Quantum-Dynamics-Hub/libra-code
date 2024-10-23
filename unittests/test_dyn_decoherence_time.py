b4788ec447ec2a962cd52dda3af00050bc37a1ce
DaehoHan <dhhan4869@gmail.com>
2024-10-23 06:27:54 -0400

    Remove redundant dependencies on numpy and data_conv

--- unittests/test_dyn_decoherence_time.py
+++ unittests/test_dyn_decoherence_time.py
@@ -1,49 +1,61 @@
 import pytest
 
+import math
 from liblibra_core import *
-
-import numpy as np
-import libra_py.data_conv as data_conv
 
 # Sets for testing edc_rates
 set1 = [
-[ np.array([[1.0 + 0.0j, 0.5 + 0.3j], [0.5 - 0.3j, 2.5 + 0.0j]]), 1.0, 1.0, 0.1, 0, np.array([[0.0, 1.3636363636363636], [1.3636363636363636, 0.0]]) ],
-[ np.array([[2.0 + 0.0j, 1.0 + 0.2j], [1.0 - 0.2j, 3.0 + 0.0j]]), 1.0, 1.0, 0.1, 0, np.array([[0.0, 0.9090909090909091], [0.9090909090909091, 0.0]]) ],
-[ np.array([[0.5 + 0.0j, 0.3 + 0.1j, 0.2 - 0.1j], [0.3 - 0.1j, 1.0 + 0.0j, 0.4 + 0.0j], [0.2 + 0.1j, 0.4 + 0.0j, 1.5 + 0.0j]]), 1.0, 1.0, 0.1, 0,  
-  np.array([[0.0, 0.45454545454545453, 0.9090909090909091], [0.45454545454545453 , 0.0, 0.45454545454545453], [ 0.9090909090909091, 0.45454545454545453, 0.0]]) ]
+[ [[1.0 + 0.0j, 0.5 + 0.3j], [0.5 - 0.3j, 2.5 + 0.0j]], 1.0, 1.0, 0.1, 0, [[0.0, 1.3636363636363636], [1.3636363636363636, 0.0]] ],
+[ [[2.0 + 0.0j, 1.0 + 0.2j], [1.0 - 0.2j, 3.0 + 0.0j]], 1.0, 1.0, 0.1, 0, [[0.0, 0.9090909090909091], [0.9090909090909091, 0.0]] ],
+[ [[0.5 + 0.0j, 0.3 + 0.1j, 0.2 - 0.1j], [0.3 - 0.1j, 1.0 + 0.0j, 0.4 + 0.0j], [0.2 + 0.1j, 0.4 + 0.0j, 1.5 + 0.0j]], 1.0, 1.0, 0.1, 0,  
+  [[0.0, 0.45454545454545453, 0.9090909090909091], [0.45454545454545453 , 0.0, 0.45454545454545453], [ 0.9090909090909091, 0.45454545454545453, 0.0]] ]
 ]
 
+# Sets for testing coherence_intervals
 set2 = [
-[ np.array([[0.8 + 0.1j], [0.6 - 0.2j]]), np.array([[0.0, 1.3636363636363636], [1.3636363636363636, 0.0]]), np.array([[1.8333333333333335], [1.128205128205128]]) ],
-[ np.array([[0.8 + 0.1j], [0.6 - 0.2j]]), np.array([[0.0, 0.9090909090909091], [0.9090909090909091, 0.0]]), np.array([[2.7500000000000000], [1.6923076923076918]]) ],
-[ np.array([[1/np.sqrt(2)], [1/np.sqrt(2)]]), np.array([[0.0, 1.3636363636363636], [1.3636363636363636, 0.0]]), np.array([[1.466666666666667], [1.466666666666667]]) ],
-[ np.array([[1/np.sqrt(2)], [1/np.sqrt(2)]]), np.array([[0.0, 0.9090909090909091], [0.9090909090909091, 0.0]]), np.array([[2.2000000000000006], [2.2000000000000006]]) ]
+[ [[0.8 + 0.1j], [0.6 - 0.2j]], [[0.0, 1.3636363636363636], [1.3636363636363636, 0.0]], [[1.8333333333333335], [1.128205128205128]] ],
+[ [[0.8 + 0.1j], [0.6 - 0.2j]], [[0.0, 0.9090909090909091], [0.9090909090909091, 0.0]], [[2.7500000000000000], [1.6923076923076918]] ],
+[ [[1/math.sqrt(2)], [1/math.sqrt(2)]], [[0.0, 1.3636363636363636], [1.3636363636363636, 0.0]], [[1.466666666666667], [1.466666666666667]] ],
+[ [[1/math.sqrt(2)], [1/math.sqrt(2)]], [[0.0, 0.9090909090909091], [0.9090909090909091, 0.0]], [[2.2000000000000006], [2.2000000000000006]] ]
 ]
 
 class TestSpecialFunctions: 
 
-    @pytest.mark.parametrize(('Hvib', 'Ekin', 'C_param', 'eps_param', 'isNBRA', 'decoh_rates_expt'), set1)
-    def test_1(self, Hvib, Ekin, C_param, eps_param, isNBRA, decoh_rates_expt):
-        nst = Hvib.shape[0]
-        Hvib = data_conv.nparray2CMATRIX(Hvib)
+    @pytest.mark.parametrize(('_Hvib', '_Ekin', '_C_param', '_eps_param', '_isNBRA', 'res_expt'), set1)
+    def test_1(self, _Hvib, _Ekin, _C_param, _eps_param, _isNBRA, res_expt):
+        nst = len(_Hvib)
+
+        Hvib = CMATRIX(nst, nst)
+        for i in range(nst):
+            for j in range(nst):
+                Hvib.set(i, j, Py2Cpp_complex([_Hvib[i][j]])[0] )
+        
+        Ekin = Py2Cpp_double([_Ekin])[0]
+        C_param = Py2Cpp_double([_C_param])[0]
+        eps_param = Py2Cpp_double([_eps_param])[0]
+        isNBRA = Py2Cpp_int([_isNBRA])[0]
 
         decoh_rates = edc_rates(Hvib, Ekin, C_param, eps_param, isNBRA)
-        decoh_rates = data_conv.MATRIX2nparray(decoh_rates)
 
-        for ist in range(nst):
-            for jst in range(nst):
-                assert abs(decoh_rates[ist, jst] - decoh_rates_expt[ist, jst]) < 1e-10
+        for i in range(nst):
+            for j in range(nst):
+                assert abs(decoh_rates.get(i, j) - res_expt[i][j]) < 1e-10
 
-    @pytest.mark.parametrize(('Coeff', 'rates', 'tau_m_expt'), set2)
-    def test_2(self, Coeff, rates, tau_m_expt):
-        nst = Coeff.shape[0]
+    @pytest.mark.parametrize(('_Coeff', '_rates', 'res_expt'), set2)
+    def test_2(self, _Coeff, _rates, res_expt):
+        nst = len(_Coeff)
         
-        Coeff = data_conv.nparray2CMATRIX(Coeff)
-        rates = data_conv.nparray2MATRIX(rates)
+        Coeff = CMATRIX(nst, 1)
+        for i in range(nst):
+            Coeff.set(i, 0, Py2Cpp_complex([_Coeff[i][0]])[0] )
+        
+        rates = MATRIX(nst, nst)
+        for i in range(nst):
+            for j in range(nst):
+                rates.set(i, j, Py2Cpp_double([_rates[i][j]])[0] )
 
         tau_m = coherence_intervals(Coeff, rates)
-        tau_m = data_conv.MATRIX2nparray(tau_m)
 
-        for ist in range(nst):
-          assert abs(tau_m[ist, 0] - tau_m_expt[ist, 0]) < 1e-10
+        for i in range(nst):
+          assert abs(tau_m.get(i, 0) - res_expt[i][0]) < 1e-10
 
