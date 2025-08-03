4c48bf1d616555c38fe03f337640d2a5feb5aa6e
Alexey Akimov <alexvakimov@gmail.com>
2025-08-03 17:02:43 -0400

    Added some standardized recipes for TSH methods

--- src/libra_py/dynamics/tsh/recipes/__init__.py
+++ src/libra_py/dynamics/tsh/recipes/__init__.py
@@ -1,5 +1,5 @@
 # ***********************************************************
-# * Copyright (C) 2020-2025 Alexey V. Akimov
+# * Copyright (C) 2025 Alexey V. Akimov
 # * This file is distributed under the terms of the
 # * GNU General Public License as published by the
 # * Free Software Foundation; either version 3 of the
@@ -7,9 +7,13 @@
 # * http://www.gnu.org/copyleft/gpl.txt
 # ***********************************************************/
 
-__all__ = ["compute",
-           "plot",
-           "recipes",
-           "recipes_Ehrenfest",
-           "save",
-           ]
+__all__ = [ "fssh_g_jt",
+            "fssh_g_minus",
+            "fssh_g_plus",
+            "fssh_h_jt",
+            "fssh_h_minus",
+            "fssh_h_plus",
+            "fssh_v_jt",
+            "fssh_v_minus",
+            "fssh_v_plus"
+          ]
