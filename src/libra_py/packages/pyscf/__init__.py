2085ff8a17a03bccf5c4581170725bab553c319f
Alexey Akimov <alexvakimov@gmail.com>
2026-04-21 14:07:13 -0400

    Added the workflow function for interfacing Libra with PySCF; unfortunately, the current version still has a bug, so not yet ready to be used

--- src/libra_py/packages/pyscf/__init__.py
+++ src/libra_py/packages/pyscf/__init__.py
@@ -4,6 +4,9 @@ from .interfaces import ElectronicStructureStrategy, MolecularGeometry
 from .implementations import CISD, CASSCF
 
 __all__ = [
+    "interfaces",
+    "implementations",
+    "methods",
     "ElectronicStructureStrategy",
     "MolecularGeometry",
     "CISD",
